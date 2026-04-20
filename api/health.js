e0a7a730572e4e43f76202b33f59bc1bdc39d692
unyor1 <armillo1221@gmail.com>
2026-04-20 22:13:52 +0800

    redo

---
+++ api/health.js
@@ -0,0 +1,15 @@
+export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
+  if (req.method !== "GET") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  return res.json({ status: "Backend is running", service: "Plant Care APIs" });
+}
